47ea68b02a07e526ef8e14f9d81d2733979e30e7
Vanessa Toro <vanetoro84@gmail.com>
2018-04-25 01:53:21 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -15,7 +15,8 @@ CREATE TABLE users (
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
+  amount INTEGER,
   user_id INTEGER,
-  project_id INTEGER,
-  amount INTEGER
+  project_id INTEGER
+
 );
--- lib/insert.sql
+++ lib/insert.sql
@@ -1,18 +1,46 @@
 INSERT INTO users (name, age) VALUES ('Vane', 33), ('Rachel', 31), ('Tania', 32), ('Jen', 38), ('Marius', 38), ('Alberto', 54), ('Brian', 6), ('Katie', 33), ('Gia', 23), ('Adriana', 51), ('Lili', 32), ('Carol', 36), ('Carlos', 55), ('Claudia', 32), ('Sarita', 35), ('Rocio', 33),
-('Yunan', 34), ('Whitney', 28), ('Sol', 29), ('Diego', 36), ('Juli', 22)
+('Yunan', 34), ('Whitney', 28), ('Sol', 29), ('Diego', 36);
 
--- INSERT INTO projects (id,title, category, funding_goal, start_date, end_date) VALUES
--- ("Vane's Bday", 'Party', 1000, '2018-01-01', '2018-03-28'),
--- ('Euro Trip', 'Travel', 10000, '2017-06-01', '2018-06-01'),
--- ('Headphones', 'Electronics', 350, '2018-03-01', '2018-03-28'),
--- ('MacBook', 'Electronics', 1500, '2018-01-01', '2018-05-01'),
--- ('Motorcycle', 'Transportation', 10000, '2018-04-01', '2018-07-30'),
--- ('Coachella', 'Party', 5000, '2018-05-01', '2018-04-01'),
--- ('BootCamp', 'Education', 9000, '2018-01-01', '2018-12-31'),
--- ('Debt Pay-Off', 'Finance', 2000, '2018-06-01', '2019-01-01'),
--- ('Tesla', 'Transportation', 80000, '2017-06-01', '2018-03-28'),
--- ('Data Science', 'Education', 9000, '2018-04-24', '2018-10-01')
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+("Vane's Bday", 'Party', 1000, '2018-01-01', '2018-03-28'),
+('Euro Trip', 'Travel', 10000, '2017-06-01', '2018-06-01'),
+('Headphones', 'Electronics', 350, '2018-03-01', '2018-03-28'),
+('MacBook', 'Electronics', 1500, '2018-01-01', '2018-05-01'),
+('Motorcycle', 'Transportation', 10000, '2018-04-01', '2018-07-30'),
+('Coachella', 'Party', 5000, '2018-05-01', '2018-04-01'),
+('BootCamp', 'Education', 9000, '2018-01-01', '2018-12-31'),
+('Debt Pay-Off', 'Finance', 2000, '2018-06-01', '2019-01-01'),
+('Tesla', 'Transportation', 80000, '2017-06-01', '2018-03-28'),
+('Data Science', 'Education', 9000, '2018-04-24', '2018-10-01');
 --
--- INSERT INTO pledges (amount, user, project) VALUES (50, 2, 1), (100, 5, 10), (30, 3, 3), (40, 1, 2), (33, 4, 9),
--- (1000, 20, 8 ), (200, 5, 6), (333, 6, 9), (121, 3, 4), (200, 10, 5), (50, 5, 5), (328, 14, 6), (614, 3, 2), (920, 16, 9)
--- (232, 12, 3), (707, 13, 5), (231, 1, 10), (10, 10, 10), (900, 19, 9), (555, 17, 7), (140, 70, 3)
+INSERT INTO pledges (amount, user_id, project_id) VALUES
+(50, 2, 1),
+(100, 5, 10),
+(30, 3, 3),
+(40, 1, 2),
+(33, 4, 9),
+(1000, 20, 8),
+(200, 5, 6),
+(333, 6, 9),
+(121, 3, 4),
+(200, 10, 5),
+(50, 5, 5),
+(328, 14, 6),
+(614, 3, 2),
+(920, 16, 9),
+(232, 12, 3),
+(707, 13, 5),
+(231, 1, 10),
+(10, 10, 10),
+(900, 19, 9),
+(555, 17, 7),
+(140, 7, 3),
+(280, 4, 6),
+(420, 8, 3),
+(840, 16, 1),
+(1200, 19, 9),
+(20, 40, 6),
+(30, 13, 7),
+(25, 15, 2),
+(1500, 20, 1),
+(150, 13, 6);
